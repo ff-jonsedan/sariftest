1f588cc5e5086cfe29e6d8101a002a0a460e34f9
Daniel Jonsén <69782709+ff-jonsedan@users.noreply.github.com>
2022-09-12 12:40:29 +0200

    add bad change

--- tf/main.tf
+++ tf/main.tf
@@ -15,3 +15,16 @@ variable "env" {
   default = ""
   description = "What Env"
 }
+
+resource "aws_kms_key" "some-key" {}
+resource "aws_efs_file_system" "sharedstore" {
+  creation_token = "asdf"
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
+  kms_key_id                      = aws_kms_key.some-key.key_id
+  encrypted                       = false
+  performance_mode                = "maxIO"
+}
